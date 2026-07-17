d7f982ac0c525005e2487e42a9ee820f0cb5cfe2
gvedika <gvedika01@gmail.com>
2026-07-18 02:18:02 +0530

    Add: surrounded-regions - submission-0

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    int x[4] = {-1, 1, 0, 0};
+    int y[4] = {0, 0, -1, 1};
+
+    bool valid(int i, int j, int n, int m) {
+        if (i < 0 || i >= n || j < 0 || j >= m)
+            return false;
+        return true;
+    }
+
+    void dfs(vector<vector<char>>& board, int i, int j, int n, int m) {
+
+        board[i][j] = '#';
+
+        for (int k = 0; k < 4; k++) {
+
+            int row = i + x[k];
+            int col = j + y[k];
+
+            if (valid(row, col, n, m) && board[row][col] == 'O') {
+                dfs(board, row, col, n, m);
+            }
+        }
+    }
+
+    void solve(vector<vector<char>>& board) {
+
+        int n = board.size();
+        int m = board[0].size();
+
+        // First column
+        for (int i = 0; i < n; i++) {
+            if (board[i][0] == 'O')
+                dfs(board, i, 0, n, m);
+        }
+
+        // Last column
+        for (int i = 0; i < n; i++) {
+            if (board[i][m - 1] == 'O')
+                dfs(board, i, m - 1, n, m);
+        }
+
+        // First row
+        for (int j = 0; j < m; j++) {
+            if (board[0][j] == 'O')
+                dfs(board, 0, j, n, m);
+        }
+
+        // Last row
+        for (int j = 0; j < m; j++) {
+            if (board[n - 1][j] == 'O')
+                dfs(board, n - 1, j, n, m);
+        }
+
+        // Convert surrounded O -> X
+        // Convert # -> O
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                if (board[i][j] == 'O')
+                    board[i][j] = 'X';
+
+                else if (board[i][j] == '#')
+                    board[i][j] = 'O';
+            }
+        }
+    }
+};
